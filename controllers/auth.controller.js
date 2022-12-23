fe43059850fd94e206333e26c5cfd70aa7a6b6ec
antoluduenabereziuk21 <antolube20@gmail.com>
2022-12-23 19:51:33 -0300

    documentation the register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,13 +2,27 @@ import { User } from "../models/User.js";
 
 const register = async (req, res) => {
    const {email, password} = req.body;
+ 
    try{
+    //remember for use of method of model of mongoose,
+    //use USer what ti is a instance of the model
     const user = new User({email, password});
     await user.save();
+
+    //jwt token 
+    
     console.log("User saved successfully");
-    res.json(user);
+    return res.json(user);
+
    }catch(error){
-    console.log(error);
+
+    console.log(error.code);
+    //here capture a error, and if user is duplicate ,
+    //response with de msg "the user already exists"
+    if(error.code === 11000){
+        return res.status(400).json({error: "Ya existe el usuario"});
+    }
+
    }
 };
 
--- models/User.js
+++ models/User.js
@@ -15,14 +15,16 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
-
+// with pre generet hash of password
 userSchema.pre("save",async function (next){
     const user = this;
-
+//here is futures when user change a password
     if(!user.isModified("password")) return next();
 
     try {
+        //salt generet a salt in hash of password
         const salt = await bcrypt.genSalt(10);
+        // hashing password 
         user.password = await bcrypt.hash(user.password, salt);
         next();
     }catch(error){
