3d441ca8c4b6f15496be617b4f79ee4771dcb14b
antoluduenabereziuk21 <antolube20@gmail.com>
2022-12-28 20:04:13 -0300

    create a index.tml for test of the login,OK

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/v1/auth',authRouter);
-
-
+//this is only used for for exampel , for use in tests of persistence of tokens
+app.use(express.static('public'))
 
 const PORT = process.env.PORT  ||5000;
 app.listen(PORT,()=> console.log(`app is running in http://localhost:${PORT}`));
